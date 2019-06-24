accfe794d359e621614159d8531510b7abad438b
Stefan Probst <stefanprobst@kolabnow.com>
2019-06-24 12:48:32 +0200

    Export colors

--- src/utils/export.js
+++ src/utils/export.js
@@ -1,5 +1,7 @@
 // TODO: Would probably be more readable if we just did string concatenation
 
+import { colors } from '../utils/api'
+
 const xmlDeclaration = '<?xml version="1.0" encoding="UTF-8"?>'
 
 export const convertToGraphML = (nodes, edges) => {
@@ -15,6 +17,20 @@ export const convertToGraphML = (nodes, edges) => {
     'xsi:schemaLocation',
     'http://graphml.graphdrawing.org/xmlns/1.0/graphml.xsd'
   )
+
+  const labelAttribute = doc.createElement('key')
+  labelAttribute.setAttribute('id', 'label')
+  labelAttribute.setAttribute('for', 'node')
+  labelAttribute.setAttribute('attr.name', 'label')
+  labelAttribute.setAttribute('attr.type', 'string')
+  graphMlElement.appendChild(labelAttribute)
+
+  const colorAttribute = doc.createElement('key')
+  colorAttribute.setAttribute('id', 'color')
+  colorAttribute.setAttribute('for', 'node')
+  colorAttribute.setAttribute('attr.name', 'color')
+  colorAttribute.setAttribute('attr.type', 'string')
+  graphMlElement.appendChild(colorAttribute)
 
   const graphElement = doc.createElement('graph')
   graphElement.setAttribute('id', 'graph')
@@ -35,6 +51,13 @@ export const convertToGraphML = (nodes, edges) => {
       const data = doc.createElement('data')
       data.setAttribute('key', 'label')
       const label = doc.createTextNode(node.label)
+      data.appendChild(label)
+      nodeElement.appendChild(data)
+    }
+    if (node.type) {
+      const data = doc.createElement('data')
+      data.setAttribute('key', 'color')
+      const label = doc.createTextNode(colors[node.type.label || node.type])
       data.appendChild(label)
       nodeElement.appendChild(data)
     }
@@ -59,16 +82,17 @@ export const convertToGexf = (nodes, edges) => {
   const doc = new Document()
 
   const gexfElement = doc.createElement('gexf')
-  gexfElement.setAttribute('xmlns', 'http://www.gexf.net/1.2draft')
-  gexfElement.setAttribute('version', '1.2')
+  gexfElement.setAttribute('xmlns', 'http://www.gexf.net/1.3draft')
+  gexfElement.setAttribute('version', '1.3')
   gexfElement.setAttribute(
     'xmlns:xsi',
     'http://www.w3.org/2001/XMLSchema-instance'
   )
   gexfElement.setAttribute(
     'xsi:schemaLocation',
-    'http://www.gexf.net/1.2draft/gexf.xsd'
+    'http://www.gexf.net/1.3draft/gexf.xsd'
   )
+  gexfElement.setAttribute('xmlns:viz', 'http://www.gexf.net/1.3draft/viz')
 
   const graphElement = doc.createElement('graph')
   graphElement.setAttribute('mode', 'static')
@@ -87,6 +111,11 @@ export const convertToGexf = (nodes, edges) => {
     nodeElement.setAttribute('id', node.id)
     if (node.label) {
       nodeElement.setAttribute('label', node.label)
+    }
+    if (node.type) {
+      const colorElement = doc.createElement('viz:color')
+      colorElement.setAttribute('hex', colors[node.type.label || node.type])
+      nodeElement.appendChild(colorElement)
     }
     nodesElement.appendChild(nodeElement)
   })
