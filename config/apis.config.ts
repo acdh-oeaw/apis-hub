e2dd906ab7432926cee324b9d9621aece8ef756c
Stefan Probst <stefanprobst@posteo.de>
2022-07-10 16:44:56 +0200

    content: set ica instance to public

--- config/apis.config.ts
+++ config/apis.config.ts
@@ -35,7 +35,7 @@ const ica: ApisInstanceConfig = {
     'The resurgence of nationalisms worldwide has reignited scholarly interest in the dissemination of ideas and cultural concepts across political and geographic borders and especially across the Atlantic. This volume is the result of an international gathering held in December 2016 at the Austrian Academy of Sciences, which was devoted to the exploration of (voluntary and enforced) transcultural migrations before, during, and after the two World Wars. In 25 incisive, wide-ranging chapters, scholars from Austria, Canada, the Czech Republic, France, Germany, Hungary, Italy, Poland, Slovenia, Spain, the United Kingdom, and the United States, revisit a century marked by international connectedness and productive cross-fertilization in the fields of literature, philosophy, science, and the arts. Taken as a whole, these essays offer a powerful antidote to new attempts to redraw the world’s boundaries according to ethnocultural dividing lines.',
   url: 'https://ica.acdh-dev.oeaw.ac.at',
   image: '/assets/images/ica.jpg',
-  access: { type: 'restricted', user: null },
+  access: { type: 'public' },
 }
 
 const mpr: ApisInstanceConfig = {
