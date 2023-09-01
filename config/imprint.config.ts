b85b5b876d04ed8152be397fddfc1dc4316517d9
Stefan Probst <stefanprobst@posteo.de>
2023-09-01 16:33:08 +0200

    refactor: use new imprint service

--- config/imprint.config.ts
+++ config/imprint.config.ts
@@ -1,9 +1,9 @@
-import { createUrl } from "@stefanprobst/request";
+import { createUrl } from '@stefanprobst/request'
 
-import { env } from "~/config/env.config";
+import { env } from '~/config/env.config'
 
 export const url = createUrl({
-	baseUrl: "https://shared.acdh.oeaw.ac.at",
-	pathname: "/acdh-common-assets/api/imprint.php",
-	searchParams: { outputLang: "en", serviceID: env.NEXT_PUBLIC_REDMINE_ID },
-});
+	baseUrl: 'https://imprint.acdh.oeaw.ac.at',
+	pathname: `/${env.NEXT_PUBLIC_REDMINE_ID}`,
+	searchParams: { locale: 'en' },
+})
